26e33da807de2e965de9a941ab2c221599b6244b
Keonwoo Kim <utophii@kaist.ac.kr>
2019-03-18 21:35:53 +0900

    save changes

--- routes/webauthn.js
+++ routes/webauthn.js
@@ -1,15 +1,51 @@
-const express   = require('express');
-const utils     = require('../utils');
-const config    = require('../config.json');
+const express = require('express');
+const utils = require('../utils');
+const config = require('../config.json');
 const base64url = require('base64url');
-const router    = express.Router();
-const database  = require('./db');
+const router = express.Router();
+const database = require('./db');
 
 /* ---------- ROUTES START ---------- */
 
+router.post('/register', (request, response) => {
+  if (!request.body || !request.body.username || !request.body.name) {
+    response.json({
+      status: 'failed',
+      message: 'Request missing name or username field!',
+    });
 
+    return;
+  }
 
+  const { username, name } = request.body;
+  if (database[username] && database[username].registered) {
+    response.json({
+      status: 'failed',
+      message: `Username ${username} already exists`,
+    });
 
+    return;
+  }
+
+  database[username] = {
+    name,
+    registered: false,
+    id: utils.randomBase64URLBuffer(),
+    authenticators: [],
+  };
+
+  const challengeMakeCred = utils.generateServerMakeCredRequest(
+    username,
+    name,
+    database[username].id
+  );
+  challengeMakeCred.status = 'ok';
+
+  request.session.challenge = challengeMakeCred.challenge;
+  request.session.username = username;
+
+  response.json(challengeMakeCred);
+});
 
 /* ---------- ROUTES END ---------- */
 
--- static/js/webauthn.auth.js
+++ static/js/webauthn.auth.js
@@ -1,2 +1,41 @@
 'use strict';
 
+$('#register').submit(function(event) {
+  event.preventDefault();
+
+  let username = this.username.value;
+  let name = this.name.value;
+
+  if (!username || !name) {
+    alert('Name or username is missing!');
+    return;
+  }
+
+  getMakeCredentialsChallenge({ username, name })
+    .then((response) => {
+      const publicKey = preformatMakeCredReq(response);
+      return navigator.credentials.create({ publicKey });
+    })
+    .then((newCred) => {
+      const makeCredResponse = publicKeyCredentialToJSON(newCred);
+      console.log(makeCredResponse);
+    });
+});
+
+let getMakeCredentialsChallenge = (formBody) => {
+  return fetch('/webauthn/register', {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formBody),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.status !== 'ok')
+        throw new Error(`error: ${response.message}`);
+
+      return response;
+    });
+};
